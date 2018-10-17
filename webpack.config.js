07af36e53bf0b603e94decb4568f3a6daa28b415
Terrijon Fowler <tfowler4@student.gsu.edu>
2018-10-16 23:39:48 -0400

    Fixed webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/react.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            { 
+                test: /\.js$/,
+                exclude: /node_modules/, 
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                } 
+            },
+            { 
+                test: /\.css$/,
+                exclude: /node_modules/, 
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                    
+                ] 
+            }
+        ]
+    },
+    watch: true,
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
+};
