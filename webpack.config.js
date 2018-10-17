128d94a4d1648baa5f461421ec0abb42cfc69d1b
Terrijon Fowler <tfowler4@student.gsu.edu>
2018-10-17 01:30:09 -0400

    removed logs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-console.log(path.resolve(__dirname, 'dist'));
-console.log(path.resolve(__dirname, 'src'));
 module.exports = {
     entry: './src/js/react.js',
     output: {
